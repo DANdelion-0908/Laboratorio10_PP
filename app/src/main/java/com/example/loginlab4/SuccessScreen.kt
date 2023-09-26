eae412eee1a1625229dec535956869379707fab8
Dan <pen22217@uvg.edu.gt>
2023-09-26 16:12:55 -0600

    Changed colors

--- app/src/main/java/com/example/loginlab4/SuccessScreen.kt
+++ app/src/main/java/com/example/loginlab4/SuccessScreen.kt
@@ -2,9 +2,13 @@ package com.example.loginlab4
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.NavigationBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 
 @Composable
